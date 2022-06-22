8c3ddfbce216095f442ff2a81e8402bf39b896c7
willowsenator <willowsenator@gmail.com>
2022-06-22 12:13:35 +0200

    refactor test and fix codacy issues

--- src/test/java/com/willow/beer_work/web/controller/BeerControllerTest.java
+++ src/test/java/com/willow/beer_work/web/controller/BeerControllerTest.java
@@ -1,8 +1,6 @@
 package com.willow.beer_work.web.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
 import com.willow.beer_work.services.BeerService;
 import com.willow.beer_work.web.model.BeerDto;
 import org.junit.jupiter.api.BeforeEach;
@@ -18,9 +16,9 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.UUID;
 
-
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.BDDMockito.given;
-import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -49,21 +47,24 @@ class BeerControllerTest {
     @Test
     void getBeer() throws Exception {
         given(beerService.getBeerById(validBeer.getId())).willReturn(validBeer);
-        mockMvc.perform(get("/api/v1/beer/" + validBeer.getId().toString()).accept(MediaType.APPLICATION_JSON))
+        var result = mockMvc.perform(get("/api/v1/beer/" + validBeer.getId().toString()).accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$.id", is(validBeer.getId().toString())));
+
+        assertNotNull(result.andReturn().getResponse());
     }
 
     @Test
     void save() throws Exception {
         String json = mapper.writeValueAsString(validBeer);
         given(beerService.save(ArgumentMatchers.any())).willReturn(validBeer);
-        mockMvc.perform(post("/api/v1/beer/")
+        var result = mockMvc.perform(post("/api/v1/beer/")
                         .content(json)
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isCreated())
                 .andExpect(header().string("Location", "/api/v1/beer/" + validBeer.getId().toString()));
+        assertNotNull(result.andReturn().getResponse().containsHeader("Location"));
     }
 
 
@@ -71,8 +72,10 @@ class BeerControllerTest {
     void update() throws Exception {
         validBeer.setId(UUID.randomUUID());
         String json = mapper.writeValueAsString(validBeer);
-        mockMvc.perform(put("/api/v1/beer/" + validBeer.getId().toString())
+        var result = mockMvc.perform(put("/api/v1/beer/" + validBeer.getId().toString())
                 .content(json).contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isNoContent());
+
+        assertNotNull(result.andReturn().getResponse());
     }
 }
